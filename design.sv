a1e595e940b506f45197501239e824d1c10811f7
augustoaccorsi <augusto.acorsi@gmail.com>
2020-04-06 17:09:48 -0300

    update code

--- design.sv
+++ design.sv
@@ -12,60 +12,80 @@ module assCPU(inAck1, inAck2, outSend1, outSend2, outData1, outData2, clkCPU, rs
   input rstCPU;
   
   /* STATES 
-  	 0 NOT SENDING
-  	 1 SENDING
+     0 NOT SENDING
+     1 SENDING
   */
   reg currentStateCPU;
-  reg nextStateCPU;
+  reg nextStateCPU;  
+  reg currentStateCPU1;
+  reg nextStateCPU1;
+  reg currentStateCPU2;
+  reg nextStateCPU2;
 
    /***** STATE *****/
     always @ (posedge clkCPU)
         begin
-            if(rstCPU == 1)
-                currentStateCPU <= 0;
-            else
-                currentStateCPU <= nextStateCPU;
+          if(rstCPU == 1) begin
+                currentStateCPU1 <= 0;
+              currentStateCPU2 <= 0;
+          end
+            else begin
+                currentStateCPU1 <= nextStateCPU1;
+              currentStateCPU2 <= nextStateCPU2;
+            end
         end
     /***** STATE *****/
 
-    /***** NEXT STATE *****/
-	always @ (*)
-	begin
-		case ({currentStateCPU})
-			0, 1:
-              if (inAck1 == 1 || inAck2 == 1) /* precisa esperar os dois acks?*/
-					nextStateCPU = 1;
-				else
-					nextStateCPU = 0;
-		endcase
-	end
+    /***** NEXT STATE CPU1 *****/
+  always @ (*)
+  begin
+      case ({currentStateCPU1})
+      0, 1:
+              if (inAck1 == 1) /* precisa esperar os dois acks?*/
+          nextStateCPU1 = 1;
+        else
+          nextStateCPU1 = 0;
+    endcase
+  end
     /***** NEXT STATE *****/
 
+      /***** NEXT STATE CPU2 *****/
+  always @ (*)
+  begin
+      case ({currentStateCPU2})
+      0, 1:
+            if (inAck2 == 1) /* precisa esperar os dois acks?*/
+          nextStateCPU2 = 1;
+        else
+          nextStateCPU2 = 0;
+    endcase
+  end
+    /***** NEXT STATE *****/
+  
+  
     /***** OUTPUT PERIFERICO 1 *****/
     always @ (*)
-	begin
-      if(rstCPU == 0 && clkCPU ==1)
-		case ({currentStateCPU})
-			0:
-			begin
+  begin
+        case ({currentStateCPU1})
+      0:
+      begin
                   outSend1 = 1;
                   // Generates a random between 0 and 15
                   outData1 = $urandom%15;
-			end
-			1:
-			begin
-				outSend1 = 0;
-			end
-			
-		endcase
-	end
-  	/***** OUTPUT PERIFERICO 1 *****/
+      end
+      1:
+      begin
+        outSend1 = 0;
+      end
+      
+    endcase
+  end
+    /***** OUTPUT PERIFERICO 1 *****/
     
     /***** OUTPUT PERIFERICO 2 *****/
     always @ (*)
-	begin
-        if(rstCPU == 0 && clkCPU ==1)
-          case ({currentStateCPU})
+  begin
+          case ({currentStateCPU2})
               0:
               begin
                     outSend2 = 1;
@@ -78,8 +98,8 @@ module assCPU(inAck1, inAck2, outSend1, outSend2, outData1, outData2, clkCPU, rs
               end
 
           endcase
-	end
-  	/***** OUTPUTS PERIFERICO 2 *****/
+  end
+    /***** OUTPUTS PERIFERICO 2 *****/
 
 endmodule
 
@@ -92,51 +112,50 @@ module assPeriferico(outAck, inSend, inData, clkPER, rstPER);
   input rstPER;
 
     /* STATES 
-  	 0 NOT RECEVING
-  	 1 RECEVING
+     0 NOT RECEVING
+     1 RECEVING
     */
   reg currentStatePER;
   reg nextStatePER;
   reg [15:0] dataPER;
 
-  	/***** STATE *****/
-	always @ (posedge clkPER)
+    /***** STATE *****/
+  always @ (posedge clkPER)
         begin
             if (rstPER == 1)
                 currentStatePER <= 0;
             else
                 currentStatePER <= nextStatePER;
         end
-	/***** STATE *****/  
+  /***** STATE *****/  
 
     /***** NEXT STATE *****/
-	always @ (*)
-	begin
-		case ({currentStatePER})
-			0, 1:
-				if (inSend == 1)
-					nextStatePER = 1;
-				else
-					nextStatePER = 0;
-		endcase
-	end
-	/***** NEXT STATE *****/
+  always @ (*)
+  begin
+    case ({currentStatePER})
+      0, 1:
+        if (inSend == 1)
+          nextStatePER = 1;
+        else
+          nextStatePER = 0;
+    endcase
+  end
+  /***** NEXT STATE *****/
 
     /***** OUTPUTS *****/
-	always @ (*)
-	begin
-      if(rstPER == 0)
-		case ({currentStatePER})	
-			0:
-				outAck = 0;
-			1:
-			begin
-				if(inSend == 1) 
-					dataPER = inData;
-					outAck = 1;
-			end
-		endcase
-	end
-  	/***** OUTPUTS *****/
+  always @ (*)
+  begin
+    case ({currentStatePER})  
+      0:
+        outAck = 0;
+      1:
+      begin
+        if(inSend == 1) 
+          dataPER = inData;
+          outAck = 1;
+      end
+    endcase
+  end
+    /***** OUTPUTS *****/
 
 endmodule
